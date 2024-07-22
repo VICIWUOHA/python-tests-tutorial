1f000ab5666e55cd2f77649b59174c3125059d5e
VICIWUOHA <viciwuoha@gmail.com>
2024-07-22 01:11:20 +0100

    update item class

--- src/item.py
+++ src/item.py
@@ -1,11 +1,23 @@
 from dataclasses import dataclass, field
-from pprint import pprint
 import uuid
 import logging
+import sqlite3
 
 
 @dataclass
 class Item:
+    """
+    Represents a sample item in an e-commerce system.
+
+    Attributes:
+        `name` (str): The name of the item.
+        `description` (str): A brief description of the item.
+        `price` (int): The price of the item in USD.
+        `sku` (str): A unique stock-keeping unit (SKU) for the item, generated automatically. 
+                     Note that this can be delegated to the database engine in prod use cases.
+
+    """
+
     name: str
     description: str
     price: int
@@ -15,10 +27,10 @@ class Item:
     def __str__(self) -> str:
         return str(
             {
+                "sku": self.sku,
                 "name": self.name,
                 "description": self.description,
                 "price": self.price,
-                "sku": self.sku,
             }
         )
 
@@ -30,28 +42,68 @@ class Item:
             return False
         return self.sku == other.sku
 
+    # Added to ensure our items never have a -ve price.
+    def __post_init__(self):
+        if self.price < 0:
+            raise ValueError(f"Item Price cannot be a negative value: {self.price}")
+
 
 class ItemRepository:
     def __init__(self) -> None:
+        self.item_count: int = 0
+        self._db_conn = sqlite3.connect("items.db")
+        self._db_cursor = self._db_conn.cursor()
+        self.__create_items_table()
         self.item_db: dict[str, Item] = {}
 
+    def __create_items_table(self) -> None:
+        self._db_cursor.execute("""CREATE TABLE IF NOT EXISTS items
+                                 (sku VARCHAR PRIMARY KEY, name TEXT, description TEXT, price INTEGER);""")
+        self._db_conn.commit()
+
     def __item_exists(self, item: Item) -> bool:
-        return item in self.item_db.values()
+        self._db_cursor.execute("SELECT 1 FROM items WHERE sku = ?", (item.sku,))
+        return self._db_cursor.fetchone() is not None
 
-    def create_item(self, item: Item) -> None:
-        self.item_db[f"{item.name}"] = item
+    def __add_item_to_db(self, item_dict: dict) -> bool:
+        try:
+            self._db_cursor.execute(
+                "INSERT INTO items VALUES (:sku, :name, :description, :price)",
+                item_dict,
+            )
+            self._db_conn.commit()
+            return True
+        except sqlite3.Error as e:
+            logging.error(f"** Error while inserting item to db: {e}")
+            return False
 
-    def get_item(self, item: Item) -> Item:
-        return self.item_db[f"{item.name}"]
+    def create_item(self, name, description, price) -> Item:
+        item = Item(name, description, price)
+        if self.__item_exists(item):
+            raise ValueError(f"{item.sku} already exists.")
+
+        if self.__add_item_to_db(item.__dict__):
+            self.item_count += 1
+
+            return item
+
+    def get_item(self, item_sku: str = None) -> Item | None:
+        if item_sku:
+            self._db_cursor.execute("SELECT * FROM items WHERE sku = ?", (item_sku,))
+            item_data = self._db_cursor.fetchone()
+            return (
+                Item(item_data[1], item_data[2], item_data[3], item_data[0])
+                if item_data
+                else None
+            )
+
+        raise KeyError("item_sku is required for the `get_item` call")
 
     def get_all_items(self) -> list[Item]:
-        return list(self.item_db.values())
+        pass
 
     def update_item(self, item: Item) -> None:
         pass
 
     def delete_item(self, item: Item) -> None:
         pass
-
-    def add_item_to_db(self, item: dict) -> None:
-        pprint(item)
