5f892a16505efef1c35c01bb1a09ad0ee6e146d4
VICIWUOHA <viciwuoha@gmail.com>
2024-07-25 02:27:46 +0100

    Basic App test.

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from src.item import Item
+from src.item import Item, ItemRepository
 from src.shopping_cart import ShoppingCart
+
+##### Dummy Script for testing App Functionality
 
 
 
@@ -18,19 +20,4 @@ item_2 = Item(
 
 item_3 = item_2
 
-print(item_2 == item_3)
-
-import sqlite3
-conn = sqlite3.connect('shop.db')
-c = conn.cursor()
-# create items table
-c.execute("""CREATE TABLE IF NOT EXISTS items
-             (name text, description text, price integer, sku text)""")
-print("Table Created successfully.")
-
-# insert items into table from item objects
-c.execute("INSERT INTO items VALUES (:name, :description, :price, :sku)", 
-          {'name': demo_item.name, 'description': demo_item.description, 'price': demo_item.price, 'sku': demo_item.sku})
-
-c.execute("SELECT * FROM items")
-print(c.fetchall())
+print(item_3 == demo_item)
