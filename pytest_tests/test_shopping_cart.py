301881be94259fc638ea726cde3d2cbfd83568bb
VICIWUOHA <viciwuoha@gmail.com>
2024-07-25 01:54:33 +0100

    :gear: format pytests with ruff

--- pytest_tests/test_shopping_cart.py
+++ pytest_tests/test_shopping_cart.py
@@ -2,6 +2,7 @@ from src.shopping_cart import ShoppingCart
 from src.item import Item
 import pytest
 
+
 # @pytest.mark.usefixtures(shopping_cart)  enables the fixture but doesn't pass it into the fxn
 def test_add_items_to_cart(shopping_cart: ShoppingCart, item: Item):
     shopping_cart.add_item(item, 40)
@@ -16,10 +17,7 @@ def test_remove_item_from_cart(shopping_cart: ShoppingCart, item: Item):
 
 @pytest.mark.skip(reason="Not Implemented Yet")
 def test_reduce_nonexistent_item_from_cart(shopping_cart: ShoppingCart, item: Item):
-    shopping_cart.add_item(
-        item=item, 
-        quantity=40
-    )
+    shopping_cart.add_item(item=item, quantity=40)
     shopping_cart.remove_cart_item(
         Item("Foozie Ergonomic Mouse", "Comfy Mouse for gamers", 50).item()
     )
