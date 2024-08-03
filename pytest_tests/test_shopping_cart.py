5c1485a2a8970afed6a577f89f0f310efaae74f0
VICIWUOHA <viciwuoha@gmail.com>
2024-08-03 12:32:25 +0100

    add test for reduce_cart_item_quantity

--- pytest_tests/test_shopping_cart.py
+++ pytest_tests/test_shopping_cart.py
@@ -15,6 +15,11 @@ def test_remove_item_from_cart(shopping_cart: ShoppingCart, item: Item):
     assert shopping_cart.cart_size == 0
 
 
+def test_reduce_item_quantity_from_cart(shopping_cart: ShoppingCart, item: Item):
+    shopping_cart.add_item(item=item, quantity=40)
+    shopping_cart.reduce_cart_item_quantity(item=item, quantity=10)
+    assert shopping_cart.cart_obj.get(item["sku"])["quantity"] == 30
+
 @pytest.mark.skip(reason="Not Implemented Yet")
 def test_reduce_nonexistent_item_from_cart(shopping_cart: ShoppingCart, item: Item):
     shopping_cart.add_item(item=item, quantity=40)
