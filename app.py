2a39987fec90b4af9642374a3ca05158553f13ae
VICIWUOHA <viciwuoha@gmail.com>
2024-08-03 13:58:54 +0100

    add example usage

--- app.py
+++ app.py
@@ -7,18 +7,27 @@ from src.shopping_cart import ShoppingCart
 
 
 demo_item = Item(
-    name="Demo Item",
-    description="This is a demo item",
+    name="Double Bass Headset",
+    description="This is a double bass headset brought to you by Vic technologies.",
     price=100,
 )
 
-
-item_2 = Item(
+demo_item_2 = Item(
     name="Demo Item 2",
     description="This is a demo item 2",
     price=200,
 )
 
-item_3 = item_2
+item_3 = demo_item_2
 
+# check equality
 print(item_3 == demo_item)
+# check other type comparison
+print(item_3 == "demo_item")
+
+my_Cart = ShoppingCart("Ewolo")
+print(my_Cart.add_item(demo_item,10))
+print(my_Cart.show_cart)
+print(my_Cart.reduce_cart_item_quantity(demo_item, 2))
+print(my_Cart.show_cart)
+print(my_Cart.remove_cart_item(demo_item))
